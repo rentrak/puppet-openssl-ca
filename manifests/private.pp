42668e875531cd879078575fdb4b4d0b0869284c
Cliff Wells <cliff.wells@rentrakmail.com>
2014-02-05 15:40:11 -0800

    ensure dir

--- manifests/private.pp
+++ manifests/private.pp
@@ -81,6 +81,9 @@ class openssl::private inherits openssl {
 
     # See the O'Reilly SSL text for more details, especially chapter four.
     file {
+      "${cadir}":
+        ensure => directory;
+
       "${cadir}/index.txt":
         require => File["${cadir}"],
         ensure => $ensure;
